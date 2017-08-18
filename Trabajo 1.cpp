f2b87e76594101446db7c496450323387f2d8148
ArmandoEmanuel <31104262+ArmandoEmanuel@users.noreply.github.com>
2017-08-17 23:57:05 -0500

    Update Trabajo 1.cpp

--- Trabajo 1.cpp
+++ Trabajo 1.cpp
@@ -16,22 +16,22 @@ using namespace std;
           cin>>sueldo;
            
         if(sueldo>=0 &&sueldo<=1000)
-            sueldo=(sueldo*118)/100;
+            sueldo*=1.18;
             
                 else
                     if(sueldo>=1001 && sueldo<=1100)
-                         sueldo=(sueldo*115)/100;
+                         sueldo*=1.15;
                             
                      else
                          if(sueldo>=1101 && sueldo<=1200)
-                            sueldo=(sueldo*112)/100;
+                            sueldo*=1.12;
                             
                             else
                                 if(sueldo>=1201 &&sueldo<=1300)
-                                    sueldo=(sueldo*110)/100;
+                                    sueldo*=1.10;
                                     
                                     else
-                                        sueldo=(sueldo*180)/100;
+                                        sueldo*=1.08;
                                              cout<<"\nTu nuevo sueldo es : "<<sueldo;
 	system("PAUSE");
         return 0;
